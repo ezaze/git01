a37dc3cc77b7d27d137befc979447f82056c506b
ezaze <zaze@qq.com>
2016-08-22 14:49:45 +0800

    搞定！

--- salt_run.py
+++ salt_run.py
@@ -1,6 +1,10 @@
-import commands
+#import commands
+import subprocess
 import sys
 print sys.argv[1]
-print '/usr/bin/salt ' + sys.argv[1] + 'cmd.run "ip a"'
-a,b = commands.getstatusoutput('/usr/bin/salt ' + sys.argv[1] + ' cmd.run "ip a"')
-print b
+subprocess.call('/usr/bin/salt ' + sys.argv[1] +' cmd.run "ip a"',shell=True)
+# subprocess.call('/usr/bin/ping -c 3 ' + sys.argv[1],shell=True)
+#subprocess.call('ping -c 3 ' + sys.argv[1], shell=True)
+#print '/usr/bin/salt ' + sys.argv[1] + 'cmd.run "ip a"'
+#a,b = commands.getstatusoutput('/usr/bin/ping ' + sys.argv[1])
+#a,b = commands.getstatusoutput('/usr/bin/salt ' + sys.argv[1] + ' cmd.run "ip a"')
